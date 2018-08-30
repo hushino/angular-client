edea653581a720a1566ceb830eeeb5e5193eef0f
Hushino <frostshock@outlook.com>
2018-08-30 10:34:02 -0300

    Fix

--- src/app/episode-by-id/episode-by-id.component.ts
+++ src/app/episode-by-id/episode-by-id.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService, Anime } from 'src/app/data.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+declare var jwplayer: any;
 @Component({
   selector: 'app-episode-by-id',
   templateUrl: './episode-by-id.component.html',
@@ -9,7 +10,9 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class EpisodeByIdComponent implements OnInit {
 
   ids = []
-
+  url = 'https://content.jwplatform.com/libraries/bfIbW5Pe.js';
+  //jwplayer: any;
+  loadAPI: Promise<any>;
   constructor(
     private dataService: DataService,
     private route: ActivatedRoute
@@ -22,6 +25,37 @@ export class EpisodeByIdComponent implements OnInit {
         console.log(data)
       })
     })
+   
+    /* setTimeout(() => {
+      this.loadAPI = new Promise((resolve) => {
+        this.loadScript();
+      });
+    }, 2000); */
   }
-
+  ngAfterViewInit() {
+    this.loadAPI = new Promise((resolve) => {
+      this.loadScript();
+    });
+  }
+  loadScript() { 
+    jwplayer('player').setup({
+      title: 'Player Test',
+      playlist: 'https://cdn.jwplayer.com/v2/media/8L4m9FJB',
+      width: 640,
+      height: 360,
+      aspectratio: '16:9',
+      mute: false,
+      autostart: true,
+      primary: 'html5',
+    });
+  }
+  /* loadScript() {
+    this.url;
+    this.jwplayer("myElement").setup({
+      "file": "https://www.w3schools.com/html/mov_bbb.mp4",
+      "image": "https://cdn.discordapp.com/attachments/423287746547810324/484563946821386240/Dlrp3ypUcAAe26w.png",
+      "height": 460,
+      "width": 540
+    });
+  } */
 }
