0351e005224ce5487905c2f7d21b70b26e2ee88a
Hushino <frostshock@outlook.com>
2018-09-01 23:33:09 -0300

    feeeeeexxxxxxx

--- src/app/anime/anime.component.ts
+++ src/app/anime/anime.component.ts
@@ -5,49 +5,28 @@ import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-anime',
   templateUrl: './anime.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./anime.component.css']
-})
-export class AnimeComponent implements OnInit {
- 
-  AnimeList2 = [];
-  animes = []
-=======
   styleUrls: ['./anime.component.scss']
 })
 export class AnimeComponent implements OnInit {
 
   AnimeList2 = [];
   animes = [];
->>>>>>> initial commit
 
   constructor(private dataService: DataService, private activeroute: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
     this.dataService.getAll().subscribe(data => {
-<<<<<<< HEAD
-      this.animes = data
-    })
-=======
       this.animes = data;
     });
->>>>>>> initial commit
 
   }
   onShowAnime(id: number) {
     this.dataService.getAnimeById(id).subscribe(datas => {
       this.AnimeList2 = datas;
      // console.log(data)
-<<<<<<< HEAD
-    })
-    this.router.navigate(['/animes', id])
-  }
- 
-=======
     });
     this.router.navigate(['/animes', id]);
   }
 
->>>>>>> initial commit
 }
